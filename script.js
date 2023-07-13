da19d51fd4069ac056c0bc193d86e842e9c8e45b
Dylan Gresham <dgresh724@gmail.com>
2023-07-12 17:59:42 -0700

    Comment function descriptions

---
+++ script.js
@@ -0,0 +1,13 @@
+/* getComputerChoice function
+ * No parameters
+ * Return either 'Rock', 'Paper', or 'Scissors' randomly */
+
+/* playRound function
+ * Two parameters: playerSelection, computerSelection
+ * Returns a string that declares the winner.
+ * Example: 'You Lose! Paper beats Rock' || 'You Win! Scissors beats Paper' */
+
+/* game function
+ * No parameters
+ * Returns the winner and loser as a string
+ * Example: 'You Lose, Computer Wins!' || 'You Win, Computer Loses!' */
