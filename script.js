08593a5fba73495d406ae543a52965a275cc68b5
Dylan Gresham <dgresh724@gmail.com>
2023-07-12 18:06:55 -0700

    Implement getComputerChoice() function

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 /* getComputerChoice function
  * No parameters
  * Return either 'Rock', 'Paper', or 'Scissors' randomly */
+function getComputerChoice() {
+    // 0 === 'Rock', 1 === 'Paper', 2 === 'Scissors'
+    let randNum = Math.floor(Math.random() * 3);
+
+    if(randNum === 0) {
+        return 'Rock'
+    } else if(randNum === 1) {
+        return 'Paper'
+    } else {
+        return 'Scissors'
+    }
+}
 
 /* playRound function
  * Two parameters: playerSelection, computerSelection
