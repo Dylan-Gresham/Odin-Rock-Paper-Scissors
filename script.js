f4f3d2871382ca89bfacc6d2c71d41cb179a9086
Dylan Gresham <dgresh724@gmail.com>
2023-07-12 18:19:23 -0700

    Implement playRound() function

--- script.js
+++ script.js
@@ -17,7 +17,29 @@ function getComputerChoice() {
 /* playRound function
  * Two parameters: playerSelection, computerSelection
  * Returns a string that declares the winner.
- * Example: 'You Lose! Paper beats Rock' || 'You Win! Scissors beats Paper' */
+ * Example: 'You Lose! Rock loses to Paper' || 'You Win! Scissors beats Paper' */
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    if(playerSelection === computerSelection) {
+        return 'It\'s a tie!'
+    } else if(playerSelection === 'rock' && computerSelection === 'scissors') {
+        return 'You Win! Rock beats Scissors';
+    } else if(playerSelection === 'rock' && computerSelection === 'paper') {
+        return 'You Lose! Rock loses to Paper';
+    } else if(playerSelection === 'paper' && computerSelection === 'rock') {
+        return 'You Win! Paper beats Rock';
+    } else if(playerSelection === 'paper' && computerSelection === 'scissors') {
+        return 'You Lose! Paper loses to Scissors';
+    } else if(playerSelection === 'scissors' && computerSelection === 'paper') {
+        return 'You Win! Scissors beats Paper';
+    } else if(playerSelection === 'scissors' && computerSelection === 'rock') {
+        return 'You Lose! Scissors loses to Rock';
+    } else {
+        return `Error! Invalid parameters:\n\tplayerSelection = ${playerSelection}\n\tcomputerSelection = ${computerSelection}`
+    }
+}
 
 /* game function
  * No parameters
