3f24fd0627b74a4e2a8496ddf725b6cc51940d8f
Dylan Gresham <dgresh724@gmail.com>
2023-07-12 18:33:21 -0700

    Implement game() function

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ function getComputerChoice() {
  * Returns a string that declares the winner.
  * Example: 'You Lose! Rock loses to Paper' || 'You Win! Scissors beats Paper' */
 function playRound(playerSelection, computerSelection) {
+    // Convert to lower case so that user input can be any combination of capitals and lower case letters
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
 
@@ -36,7 +37,7 @@ function playRound(playerSelection, computerSelection) {
         return 'You Win! Scissors beats Paper';
     } else if(playerSelection === 'scissors' && computerSelection === 'rock') {
         return 'You Lose! Scissors loses to Rock';
-    } else {
+    } else { // "Default" case in case the player enters something invalid
         return `Error! Invalid parameters:\n\tplayerSelection = ${playerSelection}\n\tcomputerSelection = ${computerSelection}`
     }
 }
@@ -45,3 +46,35 @@ function playRound(playerSelection, computerSelection) {
  * No parameters
  * Returns the winner and loser as a string
  * Example: 'You Lose, Computer Wins!' || 'You Win, Computer Loses!' */
+function game() {
+    // Declare & Instantiate variables for round stats
+    let wins = 0;
+    let losses = 0;
+    let ties = 0;
+
+    // Play all five rounds
+    for(let i = 1; i <= 5; i++) {
+        let userChoice = prompt(`Enter your choice for round ${i} of 5:`);
+        let gameDecision = playRound(userChoice, getComputerChoice());
+
+        if(gameDecision.charAt(0) === 'I') {
+            ties++;
+        } else if(gameDecision.charAt(4) === 'L') {
+            losses++;
+        } else if(gameDecision.charAt(4) === 'W') {
+            wins++;
+        } else {
+            return gameDecision;
+        }
+    }
+
+    if(wins > losses) {
+        return `Game Over, you Win!\nWins: ${wins}\nLosses: ${losses}\nTies: ${ties}`;
+    } else if(losses > wins) {
+        return `Game Over, you Lose!\nWins: ${wins}\nLosses: ${losses}\nTies: ${ties}`;
+    } else {
+        return `Game Over, it\'s a Tie!\nWins: ${wins}\nLosses: ${losses}\nTies: ${ties}`;
+    }
+}
+
+console.log(game());
